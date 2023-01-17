0f8b8e819f620cb44582550b1eb9029bd8570368
MC <chokamoses2@gmail.com>
2023-01-17 15:04:24 +0300

    Navbar and Devise Added

--- Gemfile
+++ Gemfile
@@ -11,7 +11,11 @@ gem "sprockets-rails"
 
 # Use sqlite3 as the database for Active Record
 #gem "sqlite3", "~> 1.4"
-gem "pg"
+gem "pg", "~> 1.4.5"
+
+#Add user authorization
+gem 'devise'
+
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+  get 'home/about'
+  get 'home/contact'
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
