3d0d2da93c0ba9e58ad1aa4897b980d3378c3647
MC <chokamoses2@gmail.com>
2023-01-28 21:33:13 +0300

    Associated pins with users and authenticated users

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   # GET /pins or /pins.json
   def index
     @pins = Pin.all
@@ -12,7 +13,7 @@ class PinsController < ApplicationController
 
   # GET /pins/new
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   # GET /pins/1/edit
@@ -21,7 +22,7 @@ class PinsController < ApplicationController
 
   # POST /pins or /pins.json
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
    
 
     respond_to do |format|
@@ -68,4 +69,9 @@ class PinsController < ApplicationController
     def pin_params
       params.require(:pin).permit(:description)
     end
+
+  def correct_user
+    @pin = current_user.pins.find_by(id: params[:id])
+    redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+  end
 end
